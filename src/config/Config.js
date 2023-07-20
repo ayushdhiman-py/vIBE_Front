3ec19de7a9197aa77e51465229e59f4471a6c8c7
ayushdhiman-py <ayushdhiman0107@gmail..com>
2023-07-20 22:49:21 +0530

    deploy after webpackDevServer changes

--- src/config/Config.js
+++ src/config/Config.js
@@ -5,7 +5,7 @@ import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQzlCbUp59OjWmi1gm5RA8H70U5G4BPGU",
-  authDomain: "https://vibe-wmv0.onrender.com/",
+  authDomain: "https://vIBE-eCommerce.onrender.com/",
   databaseURL: "https://vibe-d49d0-default-rtdb.firebaseio.com",
   projectId: "vibe-d49d0",
   storageBucket: "vibe-d49d0.appspot.com",
